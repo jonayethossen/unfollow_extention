c84756380b1b06dc8ff0bb2c0134c5a9a638b095
devlopersabbir <devlopersabbir@gmail.com>
2025-01-07 21:55:53 +0600

    [feat 🙂] insta follow system added 🎉

--- content.js
+++ content.js
@@ -3,23 +3,47 @@ const domains = ["instagram.com", "tiktok.com"];
 function main() {
   const currentUrl = window.location.hostname;
   if (!currentUrl) return alert("fail to get current url");
+
   if (checkHostname(domains)) {
-    setInterval(async function unfollow() {
-      const unfollowbuttons = document.getElementsByClassName(
-        "_acan _acap _acat _aj1- _ap30"
-      );
-      let index = 0;
-      if (unfollowbuttons.length <= index) return;
-      unfollowbuttons[index].click();
+    const prompt = confirm("You want to unfollow?");
 
-      new Promise((resolve, _reject) => setInterval(resolve, 1000));
+    if (!prompt) {
+      setInterval(() => {
+        const followButtons = document.getElementsByClassName(
+          " _acan _acap _acas _aj1- _ap30"
+        );
 
-      const btn = document.getElementsByClassName("_a9-- _ap36 _a9-_")[0];
-      if (btn) {
-        btn.click();
-      }
-      index++;
-    }, 500);
+        let index = 0;
+        if (followButtons.length <= index) return;
+        if (followButtons[index]) followButtons[index].click();
+        // wait 1s
+        new Promise((resolve, _reject) =>
+          setInterval(resolve, randomInteger(300, 1500))
+        );
+        index++;
+      }, 500);
+    } else {
+      // unfollow
+      setInterval(async function unfollow() {
+        const unfollowbuttons = document.getElementsByClassName(
+          "_acan _acap _acat _aj1- _ap30"
+        );
+
+        let index = 0;
+        if (unfollowbuttons.length <= index) return;
+        unfollowbuttons[index].click();
+
+        new Promise((resolve, _reject) =>
+          setInterval(resolve, randomInteger(300, 1500))
+        );
+
+        const btn = document.getElementsByClassName("_a9-- _ap36 _a9-_")[0];
+        if (btn) {
+          btn.click();
+        }
+        index++;
+      }, 500);
+    }
   }
 }
 main();
@@ -39,4 +63,7 @@ function checkHostname(domains) {
 
   // Test the hostname against the regex
   return regex.test(hostname);
+}
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
